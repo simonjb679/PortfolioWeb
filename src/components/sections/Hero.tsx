3bd8e3335f47fbfe3a6b5460210d6f71469619af
Simon Benitez <simonjb679@gmail.com>
2025-09-05 23:20:09 -0400

    Got rid of vertical oval scroll indicator

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-
 import { styles } from "../../constants/styles";
 import { ComputersCanvas } from "../canvas";
 import { config } from "../../constants/config";
@@ -25,23 +23,7 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className="xs:bottom-10 absolute bottom-32 flex w-full items-center justify-center">
-        <a href="#about">
-          <div className="border-secondary flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="bg-secondary mb-1 h-3 w-3 rounded-full"
-            />
-          </div>
-        </a>
-      </div>
+
     </section>
   );
 };
